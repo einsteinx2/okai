67c16eb150c8c281ea374e3662ef527a9f26067e
killercivilian <killercivilian@0ac42f08-cc4f-0410-a013-8b35e0a3db03>
2008-06-17 04:42:36 +0000

    git-svn-id: https://svn.code.sf.net/p/okai/code/n02@10 0ac42f08-cc4f-0410-a013-8b35e0a3db03

---
+++ src/common/StaticOrderedArray.h
@@ -0,0 +1,149 @@
+/******************************************************************************
+          .d8888b.   .d8888b.  
+         d88P  Y88b d88P  Y88b 
+         888    888        888 
+88888b.  888    888      .d88P 
+888 "88b 888    888  .od888P"  
+888  888 888    888 d88P"      
+888  888 Y88b  d88P 888"       
+888  888  "Y8888P"  888888888              Open Kaillera Arcade Netplay Library
+*******************************************************************************
+Copyright (c) Open Kaillera Team 2003-2008
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice must be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
+#pragma once
+
+#pragma intrinsic(memcpy)
+
+namespace n02 {
+
+    /*
+    Ordered static array
+    */
+
+    template <class _BaseType, int _TotalLen = 32>
+    class StaticOrderedArray
+    {
+
+    protected:
+
+        _BaseType items[_TotalLen];
+        int length;
+
+    public:
+
+        StaticOrderedArray(void)
+        {
+            length = 0;
+        }
+
+        inline void addItem(_BaseType item)
+        {
+            items[length++] = item;
+        }
+
+
+        inline void addItemPtr(_BaseType * itemPtr)
+        {
+            items[length++] = *itemPtr;
+        }
+
+        inline void insertItem(_BaseType item, int index)
+        {
+            if (index >= length) {
+                addItem(item);
+            } else {
+                memcpy(&items[index+1], &items[index], (length - index) * sizeof(_BaseType));
+                items[index] = item;
+            }
+        }
+
+        inline void insertItemPtr(_BaseType * itemPtr, int index)
+        {
+            if (index >= length) {
+                addItemPtr(itemPtr);
+            } else {
+                memcpy(&items[index+1], &items[index], (length - index) * sizeof(_BaseType));
+                items[index] = *itemPtr;
+            }
+        }
+
+        inline void removeIndex(int index)
+        {
+            if (index >= 0 && index < length) {
+                if (length-1!=index) {
+                    memcpy(&items[index], &items[index+1], (length-1-index) * sizeof(_BaseType));
+                }
+                length = length-1;
+            }
+        }
+
+        inline void removeItem(_BaseType item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (items[i] == item) {
+                    removeIndex(i);
+                    break;
+                }
+            }
+        }
+
+        inline void setItem(_BaseType item, int index)
+        {
+            if (index >=0 && index < length) {
+                items[index] = item;
+            }
+        }
+
+        inline void setItemPtr(_BaseType * item, int i)
+        {
+            if (i >=0 && i < length) {
+                items[i] = *item;
+            }
+        }
+
+        inline _BaseType getItem(int index)
+        {
+            return items[index];
+        }
+
+        inline _BaseType * getItemPtr(int index)
+        {
+            return &items[index];
+        }
+
+        inline _BaseType& operator[] (const int index)
+        {
+            return items[index];
+        }
+
+        inline void clearItems()
+        {
+            length = 0;
+        }
+
+        inline int itemsCount()
+        {
+            return length;
+        }
+
+    };
+};
